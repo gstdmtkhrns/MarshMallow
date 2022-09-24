f0a53fa539d0bb86814b13ed6d0d19f7af9426e0
gstdmtkhrns <l925144217@126.com>
2022-09-24 10:28:05 +0800

    2022.09.24

---
+++ src/main/java/com/marshmallow/MarshMallowApplication.java
@@ -0,0 +1,13 @@
+package com.marshmallow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshMallowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarshMallowApplication.class, args);
+    }
+
+}
